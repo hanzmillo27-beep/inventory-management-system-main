4d97488f8f49dbfc44de520d9b3bd912eed9429f
hanzmillo27-beep <hanzmillo27@gmail.com>
2026-03-19 00:59:07 +0900

    Add files via upload

---
+++ App/Config/Database.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Config;
+
+class Database
+{
+    public const HOST = 'localhost';
+    public const DBNAME = 'inventory_db';
+    public const USER = 'root';
+    public const PASS = '';
+    public const CHARSET = 'utf8';
+}
